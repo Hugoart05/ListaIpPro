2b4824f863d68ff4ef6010e56a8fd869ba6b2afb
Seu Nome <comercialhugo.s@gmail.com>
2025-03-27 20:56:05 -0300

    Created service user auth and repositories

---
+++ DataBase/Repository/implementations/RepositoryBase.cs
@@ -0,0 +1,62 @@
+﻿using DataBase.Config;
+using DataBase.Repository.interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase.Repository.implementations
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        private readonly DbConfig _dbConfig;
+        private readonly DbContext _context;
+
+        protected RepositoryBase(DbConfig dbConfig, DbContext context)
+        {
+            _dbConfig = dbConfig;
+            _context = context;
+        }
+
+        public async Task<T> Create(T entity)
+        {
+            var delete = _context.Entry(entity).State = EntityState.Added;
+
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<T> Delete(T entity)
+        {
+            var delete  = _context.Entry(entity).State = EntityState.Deleted;
+
+            if (entity == null)
+            {
+                return null;
+            }
+
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetById(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<T> Update(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
---
+++ DataBase/Repository/implementations/UsuarioRepository.cs
@@ -0,0 +1,19 @@
+﻿using DataBase.Config;
+using DataBase.Models;
+using DataBase.Repository.interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase.Repository.implementations
+{
+    public class UsuarioRepository : RepositoryBase<Usuario>, IUsuarioRepository
+    {
+        public UsuarioRepository(DbConfig dbConfig, DbContext context) : base(dbConfig, context)
+        {
+        }
+    }
+}
